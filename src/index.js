4382566bdc00d6dda81f73945eade0850490d910
Ogubuike Alex <ogubuikealex@mail.com>
2021-05-31 17:47:29 +0100

    Start Project

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react' ;
+import ReactDom from 'react-dom';
+import LandingPage from './pages/LandingPage/landingPage';
+
+ReactDom.render(<LandingPage />, document.getElementById("root"));
---
+++ src/pages/LandingPage/landingPage.js
@@ -0,0 +1,12 @@
+
+import React from 'react' ;
+
+const LandingPage = () => {
+    return (
+        <div>
+            ellp
+        </div>
+    )
+}
+
+export default LandingPage;
